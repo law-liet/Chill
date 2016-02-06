ac3e91141491c09df9a9dabe19b28e1bc6a2ff28
law-liet <lawliet@orion.codes>
2016-02-06 11:56:30 -0500

    [Add] some models and controllers

--- Main/models.py
+++ Main/models.py
@@ -2,16 +2,32 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    fb_token = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
     is_chilled = models.BooleanField(default=False)
     friends = models.ManyToManyField("self")
-    joined_event = models.BooleanField(default=False)
 
-class Event(models.Model):
+    def switch_chilled(self):
+        self.is_chilled = not self.is_chilled
+        self.save()
+
+class Message(models.Model):
+    sender = models.ForeignKey(User)
+    content = models.CharField(max_length=8192)
+    time = models.DateTimeField()
+
+class Group(models.Model):
     chiller = models.ForeignKey(User, related_name="chiller")
     members = models.ManyToManyField(User, related_name="members")
     name = models.CharField(max_length=64)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    size = models.IntegerField(default=2)
+    max_size = models.IntegerField(default=2)
+    size = models.IntegerField(default=1)
     description = models.CharField(max_length=8192)
+    messages = models.ManyToManyField(Message, related_name="messages")
+
+
+
--- Main/views.py
+++ Main/views.py
@@ -1,16 +1,98 @@
 from django.shortcuts import render
-from Main.models import User, Event
+from Main.models import User, Group, Message
+from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
-def create_event(request):
-    pass
+def register(request):
+    email = request.POST['email']
+    first_name = request.POST['first']
+    last_name = request.POST['last']
+    fb_token = request.POST['fb']
+    data = {}
+    try:
+        User.objects.get(email = email)
+        data['status'] = "Email Already Exist"
+        return JsonResponse(data)
+    except ObjectDoesNotExist:
+        user = User.objects.create(first_name = first_name, last_name = last_name, fb_token= fb_token, email=email)
+        user.save()
+        data['status'] = "Succeed"
+        return JsonResponse(data)
 
-def edit_event(request):
-    pass
+def get_all_chilled(request):
+    email = request.POST['email']
+    data = {}
+    try:
+        user = User.objects.get(email = email)
+        chilled_friends = user.friends.filter(is_chilled=True)
+        data['all_chilled'] = chilled_friends
+        data['status'] = "Succeed"
 
-def join_event(request):
+    except ObjectDoesNotExist:
+        data['status'] = "Cannot find User"
+
+    return JsonResponse(data)
+
+def join_chilled(request):
+    user = request.POST['email']
+    chiller = request.POST['chiller']
+    data = {}
+    try:
+        user = User.objects.get(email=user)
+        chiller = User.objects.get(email=chiller)
+        group = Group.objects.get(chiller = chiller)
+        if group.size < group.max_size:
+            group.members.add(user)
+            group.size += 1
+            group.save()
+            data['status'] = 'Succeed'
+
+        else:
+            data['status'] = 'No more chilled positions.'
+
+    except ObjectDoesNotExist:
+        data['status'] = 'The group or user does not exist'
+
+    return JsonResponse(data)
+
+def create_chill(request):
     pass
 
 def toggle_chill(request):
-    pass
+    email = request.POST['email']
+    user = User.objects.get(email = email)
+    user.switch_chilled()
 
+def send_message(request):
+    sender = request.POST['sender']
+    group = request.POST['group']
+    content = request.POST['message']
+    data = {}
+    try:
+        user = User.objects.get(email = sender)
+        group = Group.objects.get(chiller = group)
+        new_msg = Message.objects.create(sender=user,content=content)
+        new_msg.save()
+        group.messages.add(new_msg)
+        group.save()
+        data['status'] = 'Succeed'
+
+    except ObjectDoesNotExist:
+        data['status'] = 'The group or user does not exist'
+
+    return JsonResponse(data)
+
+
+def receive_message(request):
+    group = request.POST['group']
+    data = {}
+    try:
+        group = Group.objects.get(email=group)
+        data['messages'] = group.messages
+        data['status'] = 'Succeed'
+
+    except ObjectDoesNotExist:
+        data['status'] = 'Group does not exist'
+
+    return JsonResponse(data)
